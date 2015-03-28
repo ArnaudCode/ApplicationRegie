b5953dcea03888809b127ff0c24cfbd365e4c90f
ArnaudCode <jeux.arnaud@orange.fr>
2015-03-28 02:11:21 +0100

    Résolution bug de lancement depuis la vidéo

--- vue/onglet/OngletEcran.java
+++ vue/onglet/OngletEcran.java
@@ -12,12 +12,12 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 import uk.co.caprica.vlcj.player.embedded.DefaultAdaptiveRuntimeFullScreenStrategy;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
@@ -25,30 +25,27 @@ import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
  *
  * @author Arnaud
  */
-public class OngletEcran extends JPanel
-{
+public class OngletEcran extends JPanel {
+
     private static String[] IPs = {"http://192.168.1.5:8080/", "http://192.168.1.4:8080/"};
     private String description = null;
     private FullScreenVideoFrame projFrame;
     protected static String screenshotURL;
 
-    public OngletEcran()
-    {
+    public OngletEcran() {
         //this.setPreferredSize(new Dimension(Parametre.LARGEUR, Parametre.HAUTEUR));
 
         description = "Réglage des paramètres du contrôle par le public";
-
+        new NativeDiscovery().discover();
         initialisation();
     }
 
     /* Getter */
-    public String getDescription()
-    {
+    public String getDescription() {
         return description;
     }
 
-    private void initialisation()
-    {
+    private void initialisation() {
         projFrame = new FullScreenVideoFrame();
         JLabel titre = new JLabel("Ecran");
         titre.setFont(titre.getFont().deriveFont(Font.BOLD));
@@ -67,21 +64,19 @@ public class OngletEcran extends JPanel
 
         gbc.gridy++;
         gbc.gridx = 0;
-        
+
         JButton stopProj = new JButton("Arrêter la projection");
-        stopProj.addActionListener(new ActionListener()
-        {
+        stopProj.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 projFrame.stop();
                 screenshotURL = null;
             }
         });
         gbc.gridy++;
         this.add(stopProj, gbc);
-        
+
         gbc.gridwidth = 1;
         gbc.gridy++;
         this.add(new EcranPanel(0, "Rasp 1", projFrame), gbc);
@@ -90,19 +85,14 @@ public class OngletEcran extends JPanel
         this.add(new EcranPanel(1, "Rasp 1", projFrame), gbc);
 
     }
-    
-    public static BufferedImage screenshot()
-    {
+
+    public static BufferedImage screenshot() {
         BufferedImage ret = null;
-        if(screenshotURL != null)
-        {
-            try
-            {
+        if (screenshotURL != null) {
+            try {
                 URL url = new URL(screenshotURL);
                 ret = ImageIO.read(url);
-            }
-            catch(Exception ex)
-            {
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 ret = null;
             }
@@ -110,8 +100,7 @@ public class OngletEcran extends JPanel
         return ret;
     }
 
-    public class EcranPanel extends JPanel
-    {
+    public class EcranPanel extends JPanel {
 
         private String title;
         private int stream;
@@ -120,8 +109,7 @@ public class OngletEcran extends JPanel
 
         private JButton start, stop, proj;
 
-        protected EcranPanel(int stream, String title, FullScreenVideoFrame frame)
-        {
+        protected EcranPanel(int stream, String title, FullScreenVideoFrame frame) {
             super();
             this.stream = stream;
             this.title = title;
@@ -130,8 +118,7 @@ public class OngletEcran extends JPanel
             this.setVisible(true);
         }
 
-        private void initComponents()
-        {
+        private void initComponents() {
             this.setLayout(new GridBagLayout());
             GridBagConstraints cst = new GridBagConstraints();
             cst.gridx = 0;
@@ -153,15 +140,13 @@ public class OngletEcran extends JPanel
             start = new JButton("Lire");
             stop = new JButton("Arrêter");
             stop.setEnabled(false);
-            
+
             proj = new JButton("Projeter");
-            
-            start.addActionListener(new ActionListener()
-            {
+
+            start.addActionListener(new ActionListener() {
 
                 @Override
-                public void actionPerformed(ActionEvent e)
-                {
+                public void actionPerformed(ActionEvent e) {
                     play();
                     start.setEnabled(false);
                     stop.setEnabled(true);
@@ -169,12 +154,10 @@ public class OngletEcran extends JPanel
             });
             this.add(start, cst);
 
-            stop.addActionListener(new ActionListener()
-            {
+            stop.addActionListener(new ActionListener() {
 
                 @Override
-                public void actionPerformed(ActionEvent e)
-                {
+                public void actionPerformed(ActionEvent e) {
                     stop();
                     stop.setEnabled(false);
                     start.setEnabled(true);
@@ -182,53 +165,46 @@ public class OngletEcran extends JPanel
             });
             cst.gridx++;
             this.add(stop, cst);
-            
-            proj.addActionListener(new ActionListener()
-            {
+
+            proj.addActionListener(new ActionListener() {
 
                 @Override
-                public void actionPerformed(ActionEvent e)
-                {
-                    projFrame.playStream(IPs[stream]+"?action=stream");
-                    screenshotURL = IPs[stream]+"?action=snapshot";
+                public void actionPerformed(ActionEvent e) {
+                    projFrame.playStream(IPs[stream] + "?action=stream");
+                    screenshotURL = IPs[stream] + "?action=snapshot";
                 }
             });
-            
+
             cst.gridx++;
             this.add(proj, cst);
         }
-        
+
         /**
          * Lit le flux (affiché dans l'onglet).
          */
-        private void play()
-        {
+        private void play() {
             player.stop();
-            player.playMedia(IPs[0]+"?action=stream");
+            player.playMedia(IPs[0] + "?action=stream");
         }
-        
+
         /**
          * Arrête la lecture du flux.
          */
-        private void stop()
-        {
+        private void stop() {
             player.stop();
         }
     }
 
-    public class FullScreenVideoFrame extends JFrame
-    {
+    public class FullScreenVideoFrame extends JFrame {
 
         private EmbeddedMediaPlayer player;
 
-        public FullScreenVideoFrame()
-        {
+        public FullScreenVideoFrame() {
             super();
             GraphicsEnvironment ge = GraphicsEnvironment
                     .getLocalGraphicsEnvironment();
             GraphicsDevice[] gs = ge.getScreenDevices();
-            if(gs.length != 1)
-            {
+            if (gs.length != 1) {
                 this.setLocation(gs[1].getDefaultConfiguration().getBounds().getLocation());
             }
             this.setVisible(true);
@@ -240,15 +216,13 @@ public class OngletEcran extends JPanel
             this.setContentPane(mediaPlayerComponent);
             this.player.setFullScreen(true);
         }
-        
-        public void playStream(String stream)
-        {
+
+        public void playStream(String stream) {
             player.stop();
             this.player.playMedia(stream);
         }
-        
-        public void stop()
-        {
+
+        public void stop() {
             this.player.stop();
         }
     }
